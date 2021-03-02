afc4dded7dddd587602a3bf7509afc10d6ef6831
irvingdc <irvingedc@gmail.com>
2021-03-01 18:54:36 -0600

    added swedish jobs

--- src/pages/jobb.js
+++ src/pages/jobb.js
@@ -36,14 +36,66 @@ export default () => (
         }
       >
         <b style={{ color: "#f57a52" }}>
-          För tillfället finns det tyvärr inga lediga tjänster
+          Swedish Net har vunnit ett stort ramavtal inom försvarssektorn. Genom
+          avtalet stödjer vi Försvarets Materiel Verk (FMV) och Försvarsmakten
+          med projektering, utförande och dokumentation.
         </b>
         <br />
         <br />
-        Hos oss på Swedish Net jobbar bland annat projektledare, projektörer och
-        konstruktörer. Om du tror att du skulle passa bra hos oss får du gärna
-        skicka en spontanansökan till jobb@swedishnet.se. Observera att det kan
-        dröja innan du får svar på din ansökan.
+        <b style={{ color: "#f57a52" }}>
+          I projekteringsfasen tar vi fram kompletta förfrågningsunderlag där vi
+          kravställer upp till 20 olika tekniska system. Det handlar framför
+          allt om fastighetsnät, data- och kommunikationsutrustning, tekniska
+          bevakningssystem och verkstadsinredning. När dessa tekniska system ska
+          installeras stödjer vi vår kund med entreprenadsamordning av
+          upphandlade entreprenörer. Avslutningsvis jobbar vi med att ta fram
+          kompletta relationshandlingar, överlämningshandlingar samt lägger in
+          information i tillämpliga databaser.
+        </b>
+        <br />
+        <br />
+        Uppskattningsvis kommer 60 personer att jobba heltid inom avtalet. Vi
+        söker därför efter många nya medarbetare!
+        <br />
+        <br />
+        I avtalet kommer fyra roller vara aktuella: teknisk kravställare,
+        projekteringsledare, projektledare/entreprenadsamordnare samt
+        dokumentatör. FMV ställer krav på att alla som arbetar i avtalet ska ha
+        en viss typ av utbildningsbakgrund och arbetslivserfarenhet. Om man har
+        högskoleingenjörsexamen (eller högre) sänks kraven på
+        arbetslivserfarenhet.
+        <br />
+        <br />
+        Exempel: Din högsta utbildningsnivå är gymnasium, du behöver då ha minst
+        5 års arbetslivserfarenhet inom branschen för jobbet teknisk
+        kravställare. Har du däremot en högskoleingenjörsexamen (eller högre)
+        behöver du endast 2 års arbetslivserfarenhet inom branschen för jobb som
+        teknisk kravställare.
+        <br />
+        <br />
+        Olika lång arbetslivserfarenhet krävs för de olika rollerna. Klicka på
+        respektive roll för att läsa mer.
+        <br />
+        <br />
+        <Table
+          dataSource={JOBS_SV}
+          columns={[
+            {
+              title: "Roll",
+              dataIndex: "title",
+              render: (title, item) => <Link to={item.url}>{title}</Link>,
+            },
+            {
+              title: "Högsta utbildningsnivå",
+              dataIndex: "degree",
+            },
+            {
+              title: "Erfarenhet i branschen",
+              dataIndex: "experience",
+              render: (number) => `minst ${number} år`,
+            },
+          ]}
+        />
         <br />
         <br />
       </CardListItem>
---
+++ src/pages/teknisk-kravstallare.js
@@ -0,0 +1,133 @@
+import React, { Fragment } from "react";
+import { globe } from "../images";
+import CardListItem from "../components/CardListItem/CardListItem";
+import Nav from "../components/Nav/Nav";
+import Footer from "../components/Footer/Footer";
+import "../main.module.css";
+import CardListSection from "../components/CardListSection/CardListSection";
+
+export default () => (
+  <Fragment>
+    <Nav
+      lan="sv"
+      sv="/dokumentator-gymnasium/"
+      en="/en/"
+      es="/es/"
+      canonical="https://www.swedishnet.se/dokumentator-gymnasium/"
+      title="Swedish Net: Teknisk Kravställare"
+      description="Som dokumentatör kommer du jobba med att inhämta, granska och leverera installationsdokumentation efter genomförda installationer."
+    />
+    <CardListSection
+      img={globe}
+      style={{
+        background: `url(${globe}) center center / contain no-repeat`,
+        backgroundColor: "black",
+        paddingTop: "220px",
+      }}
+    >
+      <CardListItem
+        head="Teknisk Kravställare"
+        title={
+          <span>
+            <b>TEKNISK KRAVSTÄLLARE</b>
+          </span>
+        }
+      >
+        <br /> <b>Rollen:</b>
+        <br />
+        Som teknisk kravställare/projektör jobbar du i projekteringsfasen med
+        att ta fram kompletta förfrågningsunderlag. Det handlar om att ta fram
+        tekniska beskrivningar och ritningar för till exempel fastighetsnät,
+        data- och kommunikationsutrustning, tekniska bevakningssystem och
+        verkstadsinredning. Du jobbar tillsammans med en eller flera kollegor
+        samt en projekteringsledare. Rollen passar dig som har god förmåga att
+        uttrycka dig i skrift och är duktig på AutoCAD.
+        <br />
+        <br />
+        Det är viktigt att du är kommunikativ och vågar fråga – både dina
+        kollegor internt och de kunder du kommer att arbeta med.
+        <br />
+        <br />
+        <b>Din bakgrund:</b>
+        <br />{" "}
+        <ul>
+          <li>
+            Ingenjörsexamen från högskola eller universitet samt minst 2 års
+            arbetslivserfarenhet inom branschen.
+          </li>
+          <br />
+          ELLER
+          <br />
+          <br />
+          <li>
+            Avslutad gymnasieutbildning samt minst 5 års arbetslivserfarenhet
+            inom branschen.
+          </li>{" "}
+        </ul>
+        <br />
+        <b>Meriterande:</b>
+        <br /> Det är meriterande om du har kunskap eller erfarenhet inom något
+        av följande områden:
+        <br />
+        <ul>
+          <li>Fastighetsnät</li>
+          <li>Teknisk bevakning</li>
+          <li>AutoCAD</li>
+          <li>Projektering</li>
+          <li>Framtagning av förfrågningsunderlag</li>
+          <li>Upprättande av relationshandlingar</li>
+          <li>AMAEL</li>
+          <li>POINT regelverket</li>
+          <li>Entreprenadjuridik (ABK, AB, ABT).</li>
+        </ul>
+        <b>Om oss:</b>
+        <br />
+        Hos oss är arbetsmiljön viktig och vi har en trevlig, ödmjuk och
+        hjälpsam stämning på våra kontor i Hallsberg och Stockholm. Swedish Nets
+        strävan är att bli Sverige största kunskapscentrum för projektering och
+        kravställning av tekniska lösningar inom våra specialistområden.
+        <br />
+        <br />
+        <b>Registerkontroll:</b>
+        <br />
+        För arbete i avtalet krävs godkänd säkerhetsprövning. Skulle du bli
+        anställd hos oss kommer du behöva genomgå en säkerhetsprövning med
+        tillhörande registerkontroll.
+        <br />
+        <br />
+        <b>Start: </b>
+        <br />
+        Enligt överenskommelse. Rekrytering kommer ske löpande. <br />
+        <br /> <b>Omfattning: </b>
+        <br />
+        Tillsvidare (heltid) eller projektanställning.
+        <br />
+        <br /> <b>Lön: </b>
+        <br />
+        Fast marknadsmässig månadslön, individuell lönesättning.
+        <br />
+        <br /> <b>Placering:</b>
+        <br />
+        Hallsberg eller Stockholm (Munkbrogatan).
+        <br />
+        <br />
+        <b>Ansökan: </b>
+        <br />
+        Skicka CV och personligt brev till{" "}
+        <a href="mailto:jobb@swedishnet.se">jobb@swedishnet.se</a>
+        . Intervjuer och rekrytering kommer att ske löpande så vänta inte med
+        att skicka in din ansökan.
+        <br />
+        <br />
+        För frågor eller mer information om tjänsten, vänligen kontakta: <br />
+        <br />
+        <a href="mailto:jobb@swedishnet.se">jobb@swedishnet.se</a>
+        <br />
+        <br />
+        Varmt välkommen med din ansökan! <br />
+        <br />
+      </CardListItem>
+    </CardListSection>
+    <Footer lan="sv" />
+  </Fragment>
+);
